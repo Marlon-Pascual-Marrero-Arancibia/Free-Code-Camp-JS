51aae67e1b533eee90dcbc4c41d465ff2dac9e52
Marlon Pascual Marrero Arancibia <73046179+Marlon-Pascual-Marrero-Arancibia@users.noreply.github.com>
2021-01-30 18:22:40 -0500

    Update Use Conditional Logic with If Statements.js

--- Basics/Use Conditional Logic with If Statements.js
+++ Basics/Use Conditional Logic with If Statements.js
@@ -14,8 +14,8 @@ function trueOrFalse(wasThatTrue) {
   // Only change code below this line
   if (wasThatTrue)
   {
-    return true;
+    return "Yes, that was true";
   }
-  return false;
+  return "No, that was false";
   // Only change code above this line
 }
