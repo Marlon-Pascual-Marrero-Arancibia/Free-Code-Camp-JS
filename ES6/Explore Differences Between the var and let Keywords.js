902f85ca794724b3e1c0ec0cfefd29e85b1578a1
Marlon Pascual Marrero Arancibia <73046179+Marlon-Pascual-Marrero-Arancibia@users.noreply.github.com>
2021-02-14 15:10:21 -0500

    Update Explore Differences Between the var and let Keywords.js

--- ES6/Explore Differences Between the var and let Keywords.js
+++ ES6/Explore Differences Between the var and let Keywords.js
@@ -1,4 +1,20 @@
-// This is my inline comment
-/* This is a multiline comment
- * Which will span three lines
- */ 
+/*
+Intructions
+
+Update the code so it only uses the let keyword.
+
+var should not exist in the code.
+catName should be Oliver.
+quote should be "Oliver says Meow!"
+*/
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
